20608e178ace8beb1016a6a181717a0490e9f338
cientista1 <40244138+cientista1@users.noreply.github.com>
2021-02-20 17:04:02 -0300

    feat: taxaBnaUseCases

---
+++ src/useCases/taxaBnaUseCases/TaxaBnaUseController.ts
@@ -0,0 +1,18 @@
+import { Response, Request } from "express";
+import { TaxaBnaUseCase } from "./TaxaBnaUseCase";
+
+export class TaxaBnaUseController {
+  constructor(private taxaBnaUseCase: TaxaBnaUseCase) {}
+
+  async handle(req: Request, res: Response) {
+    try {
+      return res.json({
+        taxa_bna : await this.taxaBnaUseCase.execute()
+      })
+    } catch (err) {
+      return res
+        .status(400)
+        .json({ message: err.message || "Unexpected error." });
+    }
+  }
+}

48473ffeed5e7faa4d07cfd042638775e36e70b3
cientista1 <40244138+cientista1@users.noreply.github.com>
2021-02-20 16:23:13 -0300

    feat: puppeteer class

---
+++ src/services/Puppeteer.ts
@@ -0,0 +1,29 @@
+import puppeteer from "puppeteer";
+
+const chromeOptions = {
+  headless: !false,
+  args: ["--no-sandbox"],
+};
+
+export class Puppeteer {
+  public browser: any;
+  public page: any;
+  constructor(){
+    this.initialize();
+  }
+
+   private async initialize() {
+    this.browser = await puppeteer.launch(chromeOptions);
+    this.page = await this.browser.newPage();
+    await this.page.setUserAgent(
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.109 Safari/537.36"
+    );
+
+  }
+}
+
+
+
+
+
+

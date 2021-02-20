20b1738883373bd4798acb5b996d1aa20c6f486e
cientista1 <40244138+cientista1@users.noreply.github.com>
2021-02-20 20:01:00 -0300

    feat: taxa luibor

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,12 +1,17 @@
 import express from 'express';
 import {taxaBnaController} from '../useCases/taxaBnaUseCases'
+import {taxasLuiborController} from "../useCases/taxasLuiborUseCases"
 
 const router = express.Router();
-
 
 
 router.get('/taxa-de-juros/bna', async (req, res) => {
   return taxaBnaController.handle(req, res);
 })
+
+router.get('/taxa-de-juros/luibor', async (req, res) => {
+  return taxasLuiborController.handle(req, res);
+})
+
 
 export default router

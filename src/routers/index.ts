b0990d752b29abb3f478bb11e940b329b059332b
cientista1 <40244138+cientista1@users.noreply.github.com>
2021-02-20 18:05:44 -0300

    feat: index router clean

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -4,9 +4,6 @@ import {taxaBnaController} from '../useCases/taxaBnaUseCases'
 const router = express.Router();
 
 
-router.get('/', (req, res) => {
-  return res.json({hello: 'world'});
-})
 
 router.get('/taxa-de-juros/bna', async (req, res) => {
   return taxaBnaController.handle(req, res);

8d2fda24f5c37328ed6d92590c587e90571a45df
cientista1 <40244138+cientista1@users.noreply.github.com>
2021-02-21 16:37:22 -0300

    feat: rate exchange navigator

--- src/providers/implementations/RateExchangeProvider.ts
+++ src/providers/implementations/RateExchangeProvider.ts
@@ -17,20 +17,7 @@ export class RateExchangeProvider implements IRateExchangeProvider {
 
   private async extractData() {
     return await this.puppeteer.page.evaluate(() => {
-      const nodeSelectArrayRates = document.querySelectorAll(
-        "div#collapsecambios.collapse table tbody"
-      );
-
-      const nodeArrayTable = [...nodeSelectArrayRates];
-
-      let rates = [].slice.call(nodeArrayTable[0].children).map((children) => ({
-        currency: (children.children[0] as HTMLElement).innerText,
-        rate: parseFloat((children.children[1] as HTMLElement).innerText.replace(",",".")),
-      }));
-      rates.shift();
-      return {
-        rates
-      };
+     
     });
   }
 }
---
+++ src/services/implementations/RateExchangeNavigator.ts
@@ -0,0 +1,21 @@
+import {INavigator} from '../INavigator'
+
+export class RateBnaNavigator implements INavigator {
+  constructor(){}
+   navigator() {
+    const nodeSelectArrayRates = document.querySelectorAll(
+      "div#collapsecambios.collapse table tbody"
+    );
+
+    const nodeArrayTable = [...nodeSelectArrayRates];
+
+    let rates = [].slice.call(nodeArrayTable[0].children).map((children) => ({
+      currency: (children.children[0] as HTMLElement).innerText,
+      rate: parseFloat((children.children[1] as HTMLElement).innerText.replace(",",".")),
+    }));
+    rates.shift();
+    return {
+      rates
+    };
+  }
+} 

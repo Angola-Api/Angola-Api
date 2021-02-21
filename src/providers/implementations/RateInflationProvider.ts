ce7f3ad43135b2e5d606f218813b5590a2e5823f
cientista1 <40244138+cientista1@users.noreply.github.com>
2021-02-21 16:46:47 -0300

    feat: rate inflation navigator

--- src/providers/implementations/RateInflationProvider.ts
+++ src/providers/implementations/RateInflationProvider.ts
@@ -1,5 +1,5 @@
 import { RateInflation } from "../../entities/RateInflation";
-import { Puppeteer } from "../../services/Puppeteer";
+import { IPuppeteer } from "../../services/IPuppeteer";
 import { IRateInflationProvider } from "../IRateInflationProvider";
 
 export class RateInflationProvider implements IRateInflationProvider {
@@ -19,25 +19,7 @@ export class RateInflationProvider implements IRateInflationProvider {
 
   private async extractData() {
     return await this.puppeteer.page.evaluate(() => {
-      const nodeSelectData = document.getElementsByClassName("BNA-data");
-      const nodeSelectArrayRates = document.querySelectorAll(
-        "div.panel-body table tbody"
-      );
-      
-      const nodeArrayData = [...nodeSelectData];
-      const nodeArrayTable = [...nodeSelectArrayRates];
-      
-      let rates =  ([].slice.call(nodeArrayTable[0].children)).map((children) => ({
-        type : (children.children[0] as HTMLElement).innerText,
-        rate: (children.children[1] as HTMLElement).innerText,
-      }) );
-      let status = rates[0].type;
-      rates.shift();
-      return {
-        date: (nodeArrayData[0] as HTMLElement).innerText,
-        status,
-        rates
-      };
+     
     });
   }
 }
--- src/services/implementations/index.ts
+++ src/services/implementations/index.ts
@@ -2,5 +2,5 @@ export * from './Puppeteer';
 export * from './RateBnaNavigator';
 export * from './RateExchangeNavigator';
 export * from './RateForwardExchangeNavigator';
-export * from './RateExchangeNavigator';
+export * from './RateInflationNavigator';
 export * from './RateExchangeNavigator';

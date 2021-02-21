67b8b709d5a78965f54e7a5550fee514a199c3de
cientista1 <40244138+cientista1@users.noreply.github.com>
2021-02-21 02:05:13 -0300

    fix: forwardexchange rate parse float

--- src/providers/implementations/RateForwardExchangeProvider.ts
+++ src/providers/implementations/RateForwardExchangeProvider.ts
@@ -25,9 +25,10 @@ export class RateForwardExchangeProvider implements IRateForwardExchangeProvider
 
       let rates = [].slice.call(nodeArrayTable[1].children).map((children) => ({
         maturity: (children.children[0] as HTMLElement).innerText,
-        rate: parseFloat((children.children[1] as HTMLElement).innerText),
+        rate: parseFloat((children.children[1] as HTMLElement).innerText.replace(",",".")),
       }));
       rates.shift();
+      rates.pop();
       return {
         rates
       };

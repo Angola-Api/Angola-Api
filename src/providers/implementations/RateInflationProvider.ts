8c2b23b325700ce163679389e7cd465e1d566772
cientista1 <40244138+cientista1@users.noreply.github.com>
2021-02-21 16:48:12 -0300

    feat: rate inflation provider

--- src/providers/implementations/RateInflationProvider.ts
+++ src/providers/implementations/RateInflationProvider.ts
@@ -3,23 +3,11 @@ import { IPuppeteer } from "../../services/IPuppeteer";
 import { IRateInflationProvider } from "../IRateInflationProvider";
 
 export class RateInflationProvider implements IRateInflationProvider {
-  private puppeteer: Puppeteer;
-  constructor() {
-    this.puppeteer = new Puppeteer();
-  }
+
+  constructor(private puppeteer: IPuppeteer) {}
+  
    async getRateInflation(): Promise<RateInflation> {
-    await this.puppeteer.initialize();
-    await this.puppeteer.page.goto(
-      "https://www.bna.ao/Conteudos/Artigos/detalhe_artigo.aspx?idc=1017&idi=1018&idl=1"
-    );
-    const rates = await this.extractData();
-    await this.puppeteer.browser.close();
-    return rates;
+    return await this.puppeteer.extractData();
   }
 
-  private async extractData() {
-    return await this.puppeteer.page.evaluate(() => {
-     
-    });
-  }
 }

ea65911729aed507578551c529afe59df2e5b0af
cientista1 <40244138+cientista1@users.noreply.github.com>
2021-02-21 16:44:20 -0300

    feat: rate exchange navigator

--- src/providers/implementations/RateForwardExchangeProvider.ts
+++ src/providers/implementations/RateForwardExchangeProvider.ts
@@ -1,37 +1,11 @@
 import { RateForwardExchange } from "../../entities/RateForwardExchange";
-import { Puppeteer } from "../../services/Puppeteer";
+import { IPuppeteer } from "../../services/IPuppeteer";
 import { IRateForwardExchangeProvider } from "../IRateForwardExchangeProvider";
 
 export class RateForwardExchangeProvider implements IRateForwardExchangeProvider {
-  private puppeteer: Puppeteer;
-  constructor() {
-    this.puppeteer = new Puppeteer();
-  }
+  constructor(private puppeteer: IPuppeteer) {}
   async getRateForwardExchange(): Promise<RateForwardExchange> {
-    await this.puppeteer.initialize();
-    await this.puppeteer.page.goto("https://www.bna.ao/");
-    const rates = await this.extractData();
-    await this.puppeteer.browser.close();
-    return rates;
+    return await this.puppeteer.extractData();
   }
 
-  private async extractData() {
-    return await this.puppeteer.page.evaluate(() => {
-      const nodeSelectArrayRates = document.querySelectorAll(
-        "div#collapsetaxas.BNA-body-mod.collapse tbody"
-      );
-
-      const nodeArrayTable = [...nodeSelectArrayRates];
-
-      let rates = [].slice.call(nodeArrayTable[1].children).map((children) => ({
-        maturity: (children.children[0] as HTMLElement).innerText,
-        rate: parseFloat((children.children[1] as HTMLElement).innerText.replace(",",".")),
-      }));
-      rates.shift();
-      rates.pop();
-      return {
-        rates
-      };
-    });
-  }
 }
--- src/services/implementations/index.ts
+++ src/services/implementations/index.ts
@@ -1,3 +1,6 @@
 export * from './Puppeteer';
 export * from './RateBnaNavigator';
 export * from './RateExchangeNavigator';
+export * from './RateForwardExchangeNavigator';
+export * from './RateExchangeNavigator';
+export * from './RateExchangeNavigator';

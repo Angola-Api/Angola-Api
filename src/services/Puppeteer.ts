2ea21d6420c971a2d93095b6293ec346702d4da2
cientista1 <40244138+cientista1@users.noreply.github.com>
2021-02-21 15:13:01 -0300

    feat: puppeteer refactor

--- src/services/Puppeteer.ts
+++ src/services/Puppeteer.ts
@@ -10,14 +10,24 @@ export class Puppeteer {
   public page: any;
   constructor(){}
 
-    async initialize() {
+    async openBrowser(url : string) {
     this.browser = await puppeteer.launch(chromeOptions);
     this.page = await this.browser.newPage();
     await this.page.setUserAgent(
       "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.109 Safari/537.36"
     );
-
+    await this.page.goto("https://www.bna.ao/");
   }
+
+  async closeBrowser(){
+    await this.browser.close();
+  } 
+  async extractData(navigation : Function) {
+    return await this.page.evaluate(() => {
+       return navigation();
+    });
+  }
+
 }
 
 

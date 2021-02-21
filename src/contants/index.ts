303cbff2e8e893eb37dcb2ad185164d31fb4d084
adilsonfuxe <adilsonfuxe.fuxe@gmail.com>
2021-02-21 16:10:33 +0100

    feat: add getOperator

---
+++ src/contants/index.ts
@@ -0,0 +1,4 @@
+export enum Operator {
+  Movicel = 'Movicel',
+  Unitel = 'Unitel',
+}
---
+++ src/lib/implementations/get-operator.ts
@@ -0,0 +1,11 @@
+import { Operator } from '../../contants';
+import { IgetOperator } from '../iget-operator';
+export class GetOperator implements IgetOperator {
+  getOperator(phone: string): string {
+    const initial = parseInt(phone.substring(4, 6));
+    if (isNaN(initial)) return null;
+    if (initial === 91 || initial === 99) return Operator.Movicel;
+    if (initial > 91 && initial < 99) return Operator.Unitel;
+    return null;
+  }
+}

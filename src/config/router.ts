8747bdfa5527034a0fecb567c9688cbca3ff841e
cientista1 <40244138+cientista1@users.noreply.github.com>
2021-02-26 02:43:57 -0300

    feat: index page

--- src/config/router.ts
+++ src/config/router.ts
@@ -1,10 +1,13 @@
-import { Express } from 'express';
+import express, { Express } from 'express';
+import indexRouter from '../routers/IndexRouter';
 import ValidatePhoneRouter from '../routers/ValidatePhoneRouters';
 import ProvinceRouter from '../routers/ProvinceRouter';
 import MunicipioRouter from '../routers/MunicipioRoutes';
 import banksRouter from '../routers/BanksRouter';
 
 const setUpRoutes = (app: Express): void => {
+ 
+  app.use(indexRouter());
   app.use('/api/v1', banksRouter());
   app.use('/api/v1', ValidatePhoneRouter());
   app.use('/api/v1', ProvinceRouter());

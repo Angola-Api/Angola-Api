830a273ca896aae2aa8b40f403ef25d42d28c81a
Antonio Kiteque <beibiblue@hotmail.co.uk>
2023-10-18 01:00:22 +0100

    ibans

---
+++ src/lib/implementations/validateIban.spec.ts
@@ -0,0 +1,17 @@
+import { ValidateIban } from './validateIban';
+const makeSut = () => ({ sut: new ValidateIban() });
+/*describe('Validate Phone', () => {
+  it('returns true if is an Angola valid Bi', () => {
+    const { sut } = makeSut();
+    expect(sut.validate('123456789la324')).toBeTruthy();
+    expect(sut.validate('123456789LA123')).toBeTruthy();
+    expect(sut.validate('432345679LA133')).toBeTruthy();
+  });
+  it('returns false if is an Angola valid Bi', () => {
+    const { sut } = makeSut();
+    expect(sut.validate('12345678913324')).toBeFalsy();
+    expect(sut.validate('123456789L3123')).toBeFalsy();
+    expect(sut.validate('4323456793A133')).toBeFalsy();
+    expect(sut.validate('432345679LA1332')).toBeFalsy();
+  });
+});*/

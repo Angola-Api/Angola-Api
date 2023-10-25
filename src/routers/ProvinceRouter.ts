9961a03cd6d456ee29df43aab5ec5e83283f3804
elisiomualumene <elisiomualumene@gmail.com>
2023-10-25 21:06:20 +0100

    refactor: altered to a new controller with mock

--- src/routers/ProvinceRouter.ts
+++ src/routers/ProvinceRouter.ts
@@ -1,14 +1,12 @@
-import { Router } from 'express';
-import {
-  getProvinceController,
-  getOneProvinceController,
-} from '../useCases/Province';
+import { Router } from "express";
+import GetProvinceController from "../useCases/Province/getProvinciasUseCase";
+import { getOneProvinceController } from "../useCases/Province";
 
 const ProvinceRouter = (router = Router()) => {
-  router.get('/provinces', async (req, res) => {
-    return getProvinceController.handle(req, res);
+  router.get("/provinces", async (req, res) => {
+    return GetProvinceController.handle(req, res);
   });
-  router.get('/provinces/:id', async (req, res) => {
+  router.get("/provinces/:id", async (req, res) => {
     return getOneProvinceController.handle(req, res);
   });
   return router;

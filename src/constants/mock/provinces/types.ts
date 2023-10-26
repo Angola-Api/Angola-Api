4b11b75704b1ac37058d764e3ff1de75a7847def
elisiomualumene <elisiomualumene@gmail.com>
2023-10-26 21:15:55 +0100

    feat: provinces mock with his type :memo:

---
+++ src/constants/mock/provinces/types.ts
@@ -0,0 +1,10 @@
+export type IProvincesProps = {
+    id: string;
+    nome: string;
+    fundada: string;
+    capital: string;
+    area: string;
+    prefixo_telefonico: string;
+    site_governo_provincial: string;
+    municipios: string[]
+}

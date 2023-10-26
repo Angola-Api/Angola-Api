1c2fd1119bf288de6d0c119b70c5fdb5b107f9ac
elisiomualumene <elisiomualumene@gmail.com>
2023-10-26 21:18:23 +0100

    refactor: changed the instance of get one province by id

--- src/useCases/Province/getOneProvinciasUseCase/index.ts
+++ src/useCases/Province/getOneProvinciasUseCase/index.ts
@@ -1,8 +1,7 @@
 import { GetOneProvinceUseCase } from './GetOneProvinceUseCase';
-import { ProvinceMethods } from '../../../databse/methods/Provincia';
 import { GetOneProvinceController } from './GetOneProvinceController';
 
-const getOneProvinceUseCase = new GetOneProvinceUseCase(new ProvinceMethods());
+const getOneProvinceUseCase = new GetOneProvinceUseCase();
 export const getOneProvinceController = new GetOneProvinceController(
   getOneProvinceUseCase
 );

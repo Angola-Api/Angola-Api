9184320409fd68a02a6ee47146faa95c6f28bb7e
elisiomualumene <elisiomualumene@gmail.com>
2023-10-31 18:46:50 +0100

    feat: added a new environment variables

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -6,4 +6,5 @@ export const environment = {
   port: process.env.PORT || 5000,
   baseUrl: process.env.BASE_URL || "http://localhost:5000",
   biUrl: process.env.SEPE || "",
+  dbPath: process.env.SQLITE_PATH
 };
